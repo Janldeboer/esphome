8aec0a51e97738336ad77d2608146c0d527b3992
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-03-11 13:26:34 +0100

    Revert individualization

--- esphome/wizard.py
+++ esphome/wizard.py
@@ -157,7 +157,7 @@ def wizard(path):
             "or chose another configuration file.".format(color("cyan", path))
         )
         return 2
-    safe_print("Hello Jan!")
+    safe_print("Hi there!")
     sleep(1.5)
     safe_print("I'm the wizard of ESPHome :)")
     sleep(1.25)
